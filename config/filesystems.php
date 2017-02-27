c08052fc788e4b2cc88ebdb2edc1c1104d9c1a2d
浩哥 <2051672879@qq.com>
2017-02-27 11:21:19 +0800

    配置七牛上传方法

--- config/filesystems.php
+++ config/filesystems.php
@@ -63,6 +63,19 @@ return [
             'bucket' => env('AWS_BUCKET'),
         ],
 
+        'qiniu' => [
+            'driver'  => 'qiniu',
+            'domains' => [
+                'default'   => 'http://7xrxc6.com1.z0.glb.clouddn.com/', //你的七牛域名
+                'https'     => 'dn-yourdomain.qbox.me',         //你的HTTPS域名
+                'custom'    => 'static.abc.com',                //你的自定义域名
+            ],
+            'access_key'=> '',  //AccessKey
+            'secret_key'=> '',  //SecretKey
+            'bucket'    => 'test',  //Bucket名字
+            'notify_url'=> '',  //持久化处理回调地址
+        ],
+
     ],
 
 ];
